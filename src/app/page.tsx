d70173c967b23d2608fbeff99feca0369b57bbc1
Vighnesh Shenekar <115893429+vighnxsh@users.noreply.github.com>
2024-07-25 12:50:20 +0530

    25/07/24

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,79 @@
 import GradualSpacing from "@/components/magicui/gradual-spacing";
 import Link from "next/link";
 import { MdArrowOutward } from "react-icons/md";
+import {ProjectGrid} from "@/components/ProjectGrid";
+import { ChevronRight } from "lucide-react";
+ 
+import { cn } from "@/lib/utils";
+import AnimatedGradientText from "@/components/magicui/animated-gradient-text";
+ 
 
 
 export default function Home() {
   return (
 
-  <div >
- 
-  <div className="text-8xl pl-28 pt-28   text-opacity-90">
+    <div >
 
- 
-   
-   
-    <div className="pl-3">
-    <GradualSpacing
-      className=" text-8xl font-medium tracking-[-0.1em] text-sky-100  dark:text-white md:text-7xl md:leading-[5rem]"
-      text="Hello...  "
-    />
+      <div className="text-8xl pl-28 pt-28   text-opacity-90">
+
+
+
+
+        <div className="pl-3">
+          <GradualSpacing
+            className=" text-4xl font-medium tracking-[-0.1em] text-sky-100  dark:text-white md:text-7xl md:leading-[5rem]"
+            text="Hello...  "
+          />
+        </div>
+
+
+
+
+        <GradualSpacing
+          className="font-display  text-5xl font-bold tracking-[-0.1em] text-sky-900  pt-3 dark:text-white md:text-7xl md:leading-[5rem]"
+          text=" I&apos;m Vighnesh !!"
+        />
+        <div className="flex">
+        <p className="text-2xl text-sky-100 dark:text-white pt-8 pr-[10px] lg:pr-[800px]">
+          Full-stack dev here, cooking up apps with Next.js, TailWind CSS and Prisma. I do it all, from database to front-end magic. Currently diving into web3 – &apos;cause why not add more buzz to the tech soup, right? Let&apos;s build something awesome!
+
+        </p>
+        
+        <button className="text-2xl">Download my resume</button>
+
+        </div>
+
+     
+
+        <div className="flex gap-32 pt-16 text-sky-800 ">
+        
+        <Link href="#techstack">
+          <h1 className=" font-bold text-4xl  flex ">My TechStack<MdArrowOutward />
+          </h1></Link>
+          
+        <Link href="#projects">
+          <h1 className=" font-bold text-4xl  flex ">Explore my Projects<MdArrowOutward />
+          </h1></Link>
+
+          <Link href="/projects">
+          <h1 className=" font-bold text-4xl  flex ">About me<MdArrowOutward />
+          </h1></Link>
+
+          <div className="z-10 flex min-h-[16rem] items-center justify-center">
+    
     </div>
+          
 
-    
-
-
-  <GradualSpacing
-      className="font-display  text-4xl font-bold tracking-[-0.1em] text-blue-900  pt-3 dark:text-white md:text-7xl md:leading-[5rem]"
-      text=" I&apos;m Vighnesh !!"
-    />
-   
-<p className="text-2xl text-sky-100 dark:text-white pt-8 pr-[800px]">
-Full-stack dev here, cooking up apps with Next.js, TailWind CSS and Prisma. I do it all, from database to front-end magic. Currently diving into web3 – &apos;cause why not add more buzz to the tech soup, right? Let&apos;s build something awesome!
-
-</p>
-  
-   <Link href="/projects">
-   <h1 className="text-blue-100 font-bold text-4xl pt-28 flex ">Explore my Projects<MdArrowOutward />
-   </h1></Link>
- 
-
-
-   
-    
- 
-  </div>
- 
- </div>
+        </div>
+      
+      </div>  
+      {/* <section className="mt-[200px]" id="techstack">
+      Techstack
+      </section>
+      <section className="mt-[100px]" id="projects">
+        <ProjectGrid />
+      </section> */}
+      
+    </div>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 export default function page() {
   return (
     <div className='text-8xl text-white p-20 font-semibold'>
       Projects
+      <div className='mt-20 flex h-[200px] w-[200px] justify-center items-center ml-[300px] pl-[300px]'>
+      <Carousel>
+  <CarouselContent className=''>
+    <CarouselItem className='text-4xl flex items-center justify-center'>   Forum Flux</CarouselItem>
+    <CarouselItem>   project 2</CarouselItem>
+    <CarouselItem>   project 3</CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
+      </div>
     </div>
   )
 }
